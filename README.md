7f8f8fdf6ca2a89ba110c32b857752f7970a75ab
Jamie Polintan <jamiecpol@gmail.com>
2018-10-12 22:47:06 -0700

    Added purpose to README and finished looking for open devices

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Spot_PacketSniffer
 It's Spot, the commandline packet sniffer. _ha ha get it_
+
+## Why am I doing this? 
+I got curious over how wireshark worked after trying to use wireshark with my tcp proxy. 
+
+## Libraries
+libpcap - Developed this on an ubuntu vm, since I didn't want to use winpcap 
+
--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
- 
+
 #include<sys/socket.h>
 #include<arpa/inet.h>
 #include<net/ethernet.h>
@@ -23,57 +23,59 @@ int main(int argc, char *argv[]){
 
 	printf("Hello, are you here to look at some packets? \n");
 
+	char *devname;
+
 	if (argv){
 		printf("You gave me a device already, lemme snoop... \n");
 
-		char *dev = argv[1];
+		devname = argv[1];
+
+		//verify that the input is a given device 
 
 		printf("Device: %s\n", dev);
 
-		//open device.
-	}
+		
+	} else {
+		//All available devices
+		printf("Searching for available devices to snoop... \n");
 
-	//All available devices
-	printf("Searching for available devices to snoop... \n");
+		pcap_if_t *allDevices, *device;
+		pcap_t *handler; //dev handler
+		int i = 0;
+		char devs[100][100], errbuf[ERRBUFF_SIZE];
 
-	pcap_if_t *allDevices, *device;
-	pcap_t *handler; //dev handler
-	int i = 0;
-	char *devname , devs[100][100];
+		//Can't find any devices
+		if (pcap_findalldevs(&allDevices, errbuf) < 0){
+			printf("Couldn't find any available devices! \n");
+			printf("Here's some error code: %s", errbuf);
+			exit(1);
+			}
 
-	char errbuf[ERRBUFF_SIZE];
+		//List devices
+		for(device = allDevices; device != NULL; device = device->next){
+			printf("%d. %s", i++, device->name);
+			if (device->description)
+				printf(" (%s)\n", device->description);
+			else
+				printf(" (No description available for this device)\n");
+			}
 
-	if (pcap_findalldevs(&allDevices, errbuf) < 0){
-		printf("Couldn't find any available devices! \n");
-		printf("Here's some error code: %s", errbuf);
-		exit(1);
+		if (i == 0){
+			printf("\nNo interfaces found! \n");
+			return;
+			}
 
-	}
+		//Find a device to sniff
+		printf("Enter the number of the device you want to sniff : ");
+		scanf("%d" , &n);
+		devname = devs[n];
 
-	//List devices
-    for(device = allDevices; device != NULL; device = device->next)
-    {
-        printf("%d. %s", ++i, device->name);
-        if (device->description)
-            printf(" (%s)\n", device->description);
-        else
-            printf(" (No description available for this device)\n");
-    }
-    
-    if (i == 0)
-    {
-        printf("\nNo interfaces found! \n");
-        return;
-    }
-
-	//Find a device to sniff
-    printf("Enter the number of the device you want to sniff : ");
-    scanf("%d" , &n);
-	devname = devs[n];
-
+		}
 
 	//Open the device to sniff
+	printf("Opening now... \n");
+	//pcap open device? 
 
-	//Process the packet recieved
+	//Process the packet recieved - user callback function
 
 }
